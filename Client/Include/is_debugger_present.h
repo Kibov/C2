32eb5754464d730ea543581a54148f63c734900e
kibov <bartek01322@gmail.com>
2023-10-24 18:54:38 +0200

    Adding simple debugger detection

---
+++ Client/Include/is_debugger_present.h
@@ -0,0 +1,10 @@
+//
+// Created by kibkib on 24.10.2023.
+//
+#include <windows.h>
+#ifndef FERNFLOWER_IS_DEBUGGER_PRESENT_H
+#define FERNFLOWER_IS_DEBUGGER_PRESENT_H
+
+bool DebuggerPresence();
+
+#endif //FERNFLOWER_IS_DEBUGGER_PRESENT_H
---
+++ Client/Source/is_debugger_present.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kibkib on 24.10.2023.
+//
+#include "is_debugger_present.h"
+
+bool DebuggerPresence(){
+    return IsDebuggerPresent();
+}
