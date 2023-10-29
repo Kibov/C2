36e1fdb4b1a0f80d32eba19cdfbf484805e107cc
radoslaw-gasior1 <134101411+radoslaw-gasior1@users.noreply.github.com>
2023-10-29 20:46:01 +0100

    Updated database schema

--- Server/database/schema.sql
+++ Server/database/schema.sql
@@ -2,31 +2,40 @@
 DROP TABLE IF EXISTS implants;
 
 CREATE TABLE implants (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    implant_hostname TEXT NOT NULL,
-    implant_username TEXT NOT NULL,
-    implant_privilege TEXT NOT NULL,
-    ip_address TEXT NOT NULL,
-    last_seen TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    implant_id       INTEGER PRIMARY KEY,
+    implant_hostname TEXT,
+    implant_username TEXT,
+    implant_shell    CHAR(1),
+    ip_address       TEXT,
+    last_seen        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+--Stores results of the tasks executed
+DROP TABLE IF EXISTS results;
+
+CREATE TABLE results (
+    result_id           INTEGER PRIMARY KEY,
+    created             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    result              TEXT,
+    tasks_task_id       INTEGER,
+    FOREIGN KEY (tasks_task_id) REFERENCES tasks (task_id)
+);
+
+
 --Used for sending tasks to execute to a specific implant_id
 DROP TABLE IF EXISTS tasks;
 
 CREATE TABLE tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    implant_id TEXT NOT NULL,
-    command TEXT NOT NULL,
-    executed INTEGER DEFAULT 0
+    task_id             INTEGER PRIMARY KEY,
+    created             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    command             TEXT,
+    executed           CHAR(1),
+    implants_implant_id INTEGER,
+    FOREIGN KEY (implants_implant_id) REFERENCES implants (implant_id)
 );
 
---Stores results of the tasks executed 
-DROP TABLE IF EXISTS results;
-
-CREATE TABLE results (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    implant_id TEXT NOT NULL,
-    result TEXT NOT NULL
-);
-
+-- Add unique indexes for the primary keys
+CREATE UNIQUE INDEX idx_implant_id ON implants (implant_id);
+CREATE UNIQUE INDEX idx_result_id ON results (result_id);
+CREATE UNIQUE INDEX idx_task_id ON tasks (task_id);
